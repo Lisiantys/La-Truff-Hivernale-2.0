ee4b0995fe58866862b0c708ec86c646e4a1fa8a
Lisiantys <maxime.pouzargue7@gmail.com>
2022-11-12 22:42:32 +0100

    La Truff Hivernale 2.0 - V1.0

---
+++ app.js
@@ -0,0 +1,21 @@
+//Début Header Sticky
+
+window.addEventListener("scroll", () =>{
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 100);
+}) //Fin Header Sticky
+
+
+
+const menu = document.querySelector(".menu")
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
