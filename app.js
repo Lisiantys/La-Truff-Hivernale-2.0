c6995e459e4e415f62463b29c263fc3170a2a56b
Lisiantys <maxime.pouzargue7@gmail.com>
2022-11-17 23:53:41 +0100

    V9  - Header OK/ Opti img OK - HTML VALIDATOR OK -

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 //Début Header Sticky
 
-window.addEventListener("scroll", () =>{
-    let nav = document.querySelector("nav");
-    nav.classList.toggle("sticky", window.scrollY > 100);
-}) //Fin Header Sticky
+window.addEventListener("scroll", () => {
+  let nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 100);
+}); //Fin Header Sticky
 
+const menu = document.querySelector(".menu");
 
-
-const menu = document.querySelector(".menu")
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
+//Début Hamburger
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
+document.querySelectorAll(".nav-link").forEach(n => 
+n.addEventListener("click", () =>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+
+//Fin Hamburger
